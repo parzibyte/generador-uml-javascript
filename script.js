af03b5eed658710e6bd5c67e06045cf878ebba1e
Luis Cabrera Benito <contacto@parzibyte.me>
2019-02-25 17:56:09 -0600

    Quitando comentarios

--- script.js
+++ script.js
@@ -144,17 +144,3 @@ new Vue({
 		}
 	}
 });
-
-
-
-
-
-
-
-//dibujar(titulo, propiedades, metodos);
-
-
-//var link = document.createElement('a');
-//link.download = 'filename.png';
-//link.href = canvas.toDataURL()
-  //link.click();
